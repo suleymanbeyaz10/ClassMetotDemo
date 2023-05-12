25c4d1b21c86f03a4e56945d0b558129d57aa4a1
Süleyman Beyaz <suleymanbeyaz10@gmail.com>
2023-05-12 17:31:06 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    internal class Musteri
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public int Soyad { get; set; }
+
+
+    }
+}
